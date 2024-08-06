12c6845e19b5ba2bef151fd6c78461c8a488a658
modev <modev.404@gmail.com>
2024-08-06 09:24:00 +0300

    fix send request every keystroke problem using debounce function

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,23 +1,23 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams,useRouter,usePathname } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { ChangeEvent } from 'react';
-
+import { useDebouncedCallback } from "use-debounce";
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
   const { replace } = useRouter()
   const pathname = usePathname()
 
-  function handleSearch (e:ChangeEvent<HTMLInputElement>):void {
+  const  handleSearch = useDebouncedCallback((e:ChangeEvent<HTMLInputElement>)=>{
     const { value } = e.target
     const params = new URLSearchParams(searchParams)
-
+    
     value ? params.set('query',value) : params.delete('query')
-
+    
     replace(`${pathname}?${params.toString()}`)
-  }
-
+  },800)
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">

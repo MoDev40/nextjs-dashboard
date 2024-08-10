75108d663985bd52ff1ed40a8c3d5e67fa71327e
modev <modev.404@gmail.com>
2024-08-10 13:17:13 +0300

    fetch customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-    return <p>Customer Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+type SearchParams = {
+    query?: string;
+    page?:string;
+}
+
+export default function Page({ searchParams }: { searchParams: SearchParams}) {
+    const query = searchParams.query || '';
+    return <Suspense fallback={<InvoicesTableSkeleton/>}>
+        <CustomersTable query={query}/>
+    </Suspense>
 }
